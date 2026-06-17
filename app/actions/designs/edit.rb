f2551822d700d68b54718ad19dd27e0856578448
Brooke Kuhlmann <brooke@alchemists.io>
2026-06-17 11:37:36 -0600

    Added designs edit resource
    Necessary to edit an existing design. The associated screen is updated, in real-time, as changes are saved using Server Sent Events.
    
    Issue: 321
    Milestone: minor

---
+++ app/actions/designs/edit.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Actions
+    module Designs
+      # The edit action.
+      class Edit < Action
+        include Deps[:htmx_layout, template_repository: "repositories.screen_template"]
+
+        params { required(:id).filled :integer }
+
+        def handle request, response
+          parameters = request.params
+
+          halt :unprocessable_content unless parameters.valid?
+
+          response.render view,
+                          template: template_repository.find(parameters[:id]),
+                          layout: htmx_layout.call(request)
+        end
+      end
+    end
+  end
+end
